5e01bd56e01c47e5f85dcf436e3d286db3c17f0c
Jofhertongpt <josephisabnernathy@proton.me>
2025-01-28 11:16:31 -0600

    feat: import rainbow theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './styles/rainbow.css'
 import { AppProviders } from '@/components/providers/AppProviders'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
